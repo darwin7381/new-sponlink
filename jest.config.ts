326d26e05f150633dddbf14a36e9cfb1e7dd4ecf
darwin7381 <darwin7381987654@gmail.com>
2025-03-18 18:54:13 +0800

    將 Jest 配置更新為 TypeScript 格式以支持 Next.js 15

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // 指向 Next.js 應用的路徑
+  dir: './',
+});
+
+// 自定義 Jest 配置
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  clearMocks: true,
+};
+
+// 創建並導出 Jest 配置
+export default createJestConfig(config); 
