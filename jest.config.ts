97f060bb8d3c4e32193fa31729d137460f8115e0
darwin7381 <darwin7381987654@gmail.com>
2025-03-18 18:56:11 +0800

    將 jest.setup.js 更新為 ES Module 格式並重命名為 .ts 擴展名

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
