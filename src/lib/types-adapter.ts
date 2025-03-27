a696947f51cb4faf1484e2a8c4fc64baa05f7b19
darwin7381 <darwin7381987654@gmail.com>
2025-03-27 17:17:54 +0800

    恢復adaptNewEventsToOld函數

--- src/lib/types-adapter.ts
+++ src/lib/types-adapter.ts
@@ -1,10 +1,18 @@
 import { Meeting as LibMeeting, MEETING_STATUS } from './types/users';
 import { Meeting as ApiMeeting, MeetingStatus } from '@/types/meeting';
+import { Event as LibEvent } from './types/events';
+import { Event as ApiEvent } from '@/types/event';
 
 /**
  * 類型轉換適配器
  * 處理不同模塊間的類型轉換
  */
+
+// 將API的Event類型轉換為Lib的Event類型
+export function adaptNewEventsToOld(apiEvents: ApiEvent[]): LibEvent[] {
+  // 簡化版的適配器，確保類型兼容
+  return apiEvents as unknown as LibEvent[];
+}
 
 // 將API中的Meeting類型轉換為lib中的Meeting類型
 export const adaptApiMeetingsToLib = (meetings: ApiMeeting[]): LibMeeting[] => {
